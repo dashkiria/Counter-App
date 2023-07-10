803f9592758ebf8d35194a22044859863f599279
dashkiria <139123981+dashkiria@users.noreply.github.com>
2023-07-10 21:56:25 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+//vars
+let substract = document.querySelector('#substract');
+let add = document.querySelector('#add');
+console.log(substract)
+substract.addEventListener("click",function(){
+    let output = document.querySelector('#result');
+    let result = Number(output.textContent) -  1;
+    if (result>= 0 ){
+
+    }else {
+        output.textContent = result;
+    }
+    
+    console.log(output);
+});
+
+add.addEventListener("click",function(){
+    let output = document.querySelector('#result');
+    let result = Number(output.textContent) +  1;
+    output.textContent = result;
+    
+    
+    console.log(output);
+});
